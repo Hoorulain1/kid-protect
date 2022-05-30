ca930c1d913e7a0bef3e625d7abba82b253aa18c
Hoorulain1 <hoorulainameeer16@gmail.com>
2022-05-30 22:37:12 +0500

    making project work

---
+++ server.js
@@ -0,0 +1,32 @@
+// importing OpenCv library
+const cv = require('opencv4nodejs');
+const path = require('path')
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+  
+// We will now create a video capture object.
+const wCap = new cv.VideoCapture(0);
+  
+//Setting the height and width of object
+wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
+wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
+  
+// Creating get request simple route
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+  
+// Using setInterval to read the image every one second.
+setInterval(()=>{
+  
+    // Reading image from video capture device
+    const frame = wCap.read();
+  
+    // Encoding the image with base64.
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    io.emit('image', image);
+}, 1000)
+  
+server.listen(3000);
